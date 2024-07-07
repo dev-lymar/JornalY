2c7ce0a771bcef8dc5cf7182856582fc34ddfb78
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-07 22:54:19 +0200

    Created and registered new users app

--- jornaly/urls.py
+++ jornaly/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls', namespace='posts'))
+    path('auth/', include('users.urls', namespace='users')),
+    path('', include('posts.urls', namespace='posts')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
