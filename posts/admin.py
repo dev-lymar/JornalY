29dcc1bf078142fb9fbfa0f2f8aa7675d21edfa1
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-07 19:53:47 +0200

    Fixed index and group list templates, added display group posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Group
+
+
+admin.site.register(Group)
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group',)
     search_fields = ('text',)
     list_filter = ('pub_date',)
+    list_editable = ('group',)
     empty_value_display = '-empty-'
--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('group/<slug:slug>/', views.group_posts, name='groups'),
+    path('group/<slug:slug>/', views.group_posts, name='group'),
 ]
