47ad38cae3ff85f5ba7b6aa3c62c1bdcc3ad3d33
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-07 15:05:08 +0200

    Added urls and view functions

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_posts(request):
+    return HttpResponse('Посты')
+
+
+def group_detail(request, slug):
+    return HttpResponse(f'Пост - {slug}')
