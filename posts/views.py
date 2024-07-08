0e01986cc94c36fe3dee1944c60daf1bf911cc8f
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-08 12:30:58 +0200

    Added pagination, paginator template, fix index view, #commented search

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from .models import Post, Group
 
 
 def index(request):
+    post_list = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(post_list, 10)
+
+    page_number = request.GET.get('page')
+
+    page_obj = paginator.get_page(page_number)
+
     keyword = request.GET.get('q', None)
     if keyword:
         posts = Post.objects.filter(text__contains=keyword).select_related('author').select_related('group')
@@ -11,6 +18,7 @@ def index(request):
         posts = None
     context = {
         'title': 'Search by post',
+        'page_obj': page_obj,
         'posts': posts,
     }
     return render(request, 'posts/index.html', context=context)
--- users/views.py
+++ users/views.py
@@ -5,5 +5,5 @@ from .forms import CreationForm
 
 class SignUpView(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy('posts:index')
+    success_url = reverse_lazy('posts:home')
     template_name = 'users/signup.html'
