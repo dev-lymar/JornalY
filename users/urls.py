841289778a13a55e29670541131ada7085595a01
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-10 10:28:09 +0200

    Added password reset templates, password reset link in header

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path
 from .views import SignUpView
 
@@ -12,4 +12,14 @@ urlpatterns = [
       LogoutView.as_view(template_name='users/logged_out.html'),
       name='logout'
     ),
+    path('password_reset/',
+         PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
