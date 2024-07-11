fc6baee6cf82a5a8fc9751c33e11ffd64412a970
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-11 23:42:36 +0200

    Edited posts urls tests and create users tests urls

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,10 +1,49 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
+
+from posts.models import Post, Group
+
+User = get_user_model()
 
 
 class StaticURLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='testuser', password='12345')
+        cls.group = Group.objects.create(title='Test Group', slug='test-group', description='Test description')
+        cls.post = Post.objects.create(
+            id=100,
+            text='Test text',
+            author=cls.user,
+            group=cls.group
+        )
+
     def setUp(self):
         self.guest_client = Client()
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
 
     def test_homepage(self):
         response = self.guest_client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_group_page(self):
+        response = self.guest_client.get('/group/test-slug')
+        self.assertEqual(response.status_code, 301)
+
+    def test_user_profile_page(self):
+        response = self.guest_client.get('/profile/testuser')
+        self.assertEqual(response.status_code, 301)
+
+    def test_post_page(self):
+        response = self.guest_client.get('/posts/100/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_edit_page_only_for_author(self):
+        response = self.authorized_client.get('/posts/100/edit/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_create_only_for_authorized_user(self):
+        response = self.authorized_client.get('/create/')
         self.assertEqual(response.status_code, 200)
---
+++ users/tests/test_urls.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+User = get_user_model()
+
+
+class StaticURLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='testuser', password='12345')
+
+    def setUp(self):
+        self.guest_client = Client()
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+    def test_login_page(self):
+        response = self.guest_client.get('/auth/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page(self):
+        response = self.guest_client.get('/auth/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_password_change_only_for_authorized_user(self):
+        response = self.authorized_client.get('/auth/password_change/')
+        self.assertEqual(response.status_code, 200)
