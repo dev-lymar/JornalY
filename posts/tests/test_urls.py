8cb593f354d1b24f68dab1d9d7055ffa7f394660
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-12 09:08:08 +0200

    Added tests for posts views, fixed tests posts urls, edit requirements

--- posts/tests/test_urls.py
+++ posts/tests/test_urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
+from django.urls import reverse
 
 from posts.models import Post, Group
 
@@ -25,25 +26,31 @@ class StaticURLTests(TestCase):
         self.authorized_client.force_login(self.user)
 
     def test_homepage(self):
+        """Checking if the homepage is available."""
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_group_page(self):
-        response = self.guest_client.get('/group/test-slug')
-        self.assertEqual(response.status_code, 301)
+        """"Check the availability of the group page and redirect to the correct URL."""
+        response = self.guest_client.get(reverse('posts:group_list', kwargs={'slug': 'test-group'}))
+        self.assertEqual(response.status_code, 200)
 
     def test_user_profile_page(self):
-        response = self.guest_client.get('/profile/testuser')
-        self.assertEqual(response.status_code, 301)
+        """Check if the user profile page is available and redirect to the correct URL."""
+        response = self.guest_client.get(reverse('posts:profile', kwargs={'username': 'testuser'}))
+        self.assertEqual(response.status_code, 200)
 
     def test_post_page(self):
-        response = self.guest_client.get('/posts/100/')
+        """Checking if the post page is available."""
+        response = self.guest_client.get(reverse('posts:post_detail', kwargs={'post_id': 100}))
         self.assertEqual(response.status_code, 200)
 
     def test_post_edit_page_only_for_author(self):
-        response = self.authorized_client.get('/posts/100/edit/')
+        """Check if the post edit page is available for the author only."""
+        response = self.authorized_client.get(reverse('posts:post_edit', kwargs={'pk': 100}))
         self.assertEqual(response.status_code, 200)
 
     def test_post_create_only_for_authorized_user(self):
-        response = self.authorized_client.get('/create/')
+        """"Check if the post creation page is only available to an authorised user."""
+        response = self.authorized_client.get(reverse('posts:post_create'))
         self.assertEqual(response.status_code, 200)
---
+++ posts/tests/test_views.py
@@ -0,0 +1,62 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from django import forms
+
+from posts.models import Post, Group
+
+User = get_user_model()
+
+
+class PostPagesTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='testuser', password='12345')
+        cls.group = Group.objects.create(title='Test Group', slug='test-group', description='Test description')
+        cls.post = Post.objects.create(
+            id=100,
+            text='Test text',
+            author=cls.user,
+            group=cls.group
+        )
+
+    def setUp(self):
+        self.guest_client = Client()
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+    def test_pages_uses_correct_template(self):
+        """The URL uses the appropriate template."""
+        templates_pages_names = {
+            'posts/index.html': reverse('posts:home'),
+            'posts/group_list.html': reverse('posts:group_list', kwargs={'slug': 'test-group'}),
+            'posts/profile.html': reverse('posts:profile', kwargs={'username': 'testuser'}),
+            'posts/post_detail.html': reverse('posts:post_detail', kwargs={'post_id': 100}),
+            'posts/create_post.html': reverse('posts:post_edit', kwargs={'pk': 100}),
+            'posts/create_post.html': reverse('posts:post_create'),
+        }
+        for template, reverse_name in templates_pages_names.items():
+            with self.subTest(reverse_name=reverse_name):
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
+
+    def test_home_page_show_correct_context(self):
+        """The home template is formed with the correct context."""
+        """The home template is formed with the correct context."""
+        response = self.authorized_client.get(reverse('posts:home'))
+        self.assertEqual(response.context['title'], 'Search by post')
+
+    def test_pagination(self):
+        """The home page paginates posts correctly."""
+        posts = [
+            Post(text=f'Test text {i}', author=self.user, group=self.group)
+            for i in range(14)
+        ]
+        Post.objects.bulk_create(posts)
+
+        response = self.authorized_client.get(reverse('posts:home'))
+        self.assertEqual(len(response.context['page_obj']), 10)
+
+        response = self.authorized_client.get(reverse('posts:home') + '?page=2')
+        self.assertEqual(len(response.context['page_obj']), 5)
