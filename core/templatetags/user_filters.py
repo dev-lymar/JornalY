fa63efe46b4e23bd10068a9e57de475a89973e52
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-13 11:01:38 +0200

    Added tests for comments, fixed user_filters

--- core/templatetags/user_filters.py
+++ core/templatetags/user_filters.py
@@ -3,12 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='addclass')
+@register.filter
 def addclass(field, css):
-    classes, attrs = css.split('&')
-    attrs = attrs.strip().split()
-    class_dict = {'class': classes.strip()}
-    for attr in attrs:
-        key, value = attr.split('=')
-        class_dict[key.strip()] = value.strip()
-    return field.as_widget(attrs=class_dict)
+    return field.as_widget(attrs={'class': css})
